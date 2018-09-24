576073fc853c8e1ec60c4b83e3c86749c60f5b80
Andriana Kovalyshyn <Andriana_Kovalyshyn@epam.com>
2018-09-24 11:13:57 +0300

    parameters validation added

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: process.env.DB_CLIENT || 'postgresql',
     connection: {
-      user: process.env.DB_USER || 'ankvellia',
+      user: process.env.DB_USER || 'reservation',
       password: process.env.DB_PASSWORD || 'reservation',
       database: process.env.DB_NAME || 'reservation',
       host: process.env.DB_HOST || '127.0.0.1'
